152e401838c34ef1490f3a341281f59c8f527fe4
andremaesha <andremaesha@gmail.com>
2025-06-09 16:03:20 +0700

    Add unit test for `CustomerTopup` method in Snap client.

--- snap/client_test.go
+++ snap/client_test.go
@@ -140,3 +140,42 @@ func TestClient_HistoryList(t *testing.T) {
 
 	println("response: ", string(bytes))
 }
+
+func TestClient_CustomerTopup(t *testing.T) {
+	privateKey, err := os.ReadFile("../certs/enc_stg.key")
+	if err != nil {
+		t.Fatalf("Failed to read private key: %v", err)
+	}
+
+	client, err := NewClient("99999", privateKey)
+	if err != nil {
+		t.Fatalf("Failed to create client: %v", err)
+	}
+	err = client.SetEnv("prod")
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := client.CustomerTopup(context.Background(), &CustomerTopupRequest{
+		PartnerReferenceNo: "20250609150352617",
+		CustomerNumber:     "0812254830",
+		Amount: &Amount{
+			Value:    "76860.00",
+			Currency: "IDR",
+		},
+		TransactionDate: "2025-06-09T15:03:52+07:00",
+		AdditionalInfo: &AdditionalInfoCustomerTopupRequest{
+			SourceAccount:          "9920017573",
+			PlatformCode:           "gpy",
+			InstructDate:           "",
+			BeneficiaryEmail:       "aanfaspay2022@gmail.com,aan28setiawan@gmail.com",
+			TransactionDescription: "Tunjangan Pulsa 20250609",
+			CallbackUrl:            "https://245e-103-83-94-10.ngrok-free.app/v1/snap/callback",
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	println(res.ResponseMessage)
+}
