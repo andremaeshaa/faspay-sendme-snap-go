cb267399ef966e4e0f590e3699c48292ab8478ea
andremaesha <andremaesha@gmail.com>
2025-06-09 11:26:53 +0700

    Add `SetEnv` method and `TransferInterBank` functionality to Snap client to support interbank transfers and dynamic environment setup.

--- snap/services.go
+++ snap/services.go
@@ -2,8 +2,29 @@ package snap
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 )
+
+type Services interface {
+	SetEnv(envType string) error
+	AccountInquiry(ctx context.Context, request *ExternalAccountInquiryRequest) (*ExternalAccountInquiryResponse, error)
+	TransferInterBank(ctx context.Context, request *TransferInterBankRequest) (*TransferInterBankResponse, error)
+}
+
+// SetEnv sets the environment for the client, switching the base URL between "sandbox" and "prod" environments.
+func (c *Client) SetEnv(envType string) error {
+	if envType == "sandbox" {
+		c.baseURL = baseUrlSandbox
+		c.environment = "sandbox"
+	} else if envType == "prod" {
+		c.baseURL = baseUrlProd
+		c.environment = "prod"
+	} else {
+		return fmt.Errorf("invalid env type")
+	}
+	return nil
+}
 
 // AccountInquiry performs an inquiry for external account details
 func (c *Client) AccountInquiry(ctx context.Context, request *ExternalAccountInquiryRequest) (*ExternalAccountInquiryResponse, error) {
@@ -18,6 +39,25 @@ func (c *Client) AccountInquiry(ctx context.Context, request *ExternalAccountInq
 
 	// The API directly returns the account inquiry response without a wrapper
 	var response ExternalAccountInquiryResponse
+
+	if err := c.parseResponse(resp, &response); err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
+
+func (c *Client) TransferInterBank(ctx context.Context, request *TransferInterBankRequest) (*TransferInterBankResponse, error) {
+	if c.environment == "sandbox" {
+		println("Info: Transaction will be processed in sandbox mode")
+	}
+
+	resp, err := c.doRequest(ctx, http.MethodPost, EndpointTransferInterbank, request)
+	if err != nil {
+		return nil, err
+	}
+
+	var response TransferInterBankResponse
 
 	if err := c.parseResponse(resp, &response); err != nil {
 		return nil, err
